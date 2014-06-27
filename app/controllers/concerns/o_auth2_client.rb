e1d80faaab6c218c0a8ce182bdb11036fcf4e932
Brent Kastner <brentkastner@gmail.com>
2014-06-26 20:02:40 -0500

    remove staging reference

--- app/controllers/concerns/o_auth2_client.rb
+++ app/controllers/concerns/o_auth2_client.rb
@@ -4,7 +4,7 @@ module OAuth2Client
   included do
    def return_access_token()
      @token = session["token"]
-     client ||= OAuth2::Client.new(ENV["ACCLAIM_ID"], ENV["ACCLAIM_SECRET"], :site => "https://jefferson-staging.herokuapp.com")
+     client ||= OAuth2::Client.new(ENV["ACCLAIM_ID"], ENV["ACCLAIM_SECRET"], :site => "https://www.youracclaim.com")
      @access_token ||= OAuth2::AccessToken.new(client, @token)
    end
   end
